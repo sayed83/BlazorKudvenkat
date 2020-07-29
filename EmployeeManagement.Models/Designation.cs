189d5378ca26b516008832a4c3e43c3feb130d5b
sayed83 <mdsayed83@gmail.com>
2020-07-29 19:45:25 +0600

    Designation

---
+++ EmployeeManagement.Models/Designation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeManagement.Models
+{
+    class Designation
+    {
+        public int DesigId { get; set; }
+        public string DesigName { get; set; }
+        public int EmployeeId { get; set; }
+    }
+}
